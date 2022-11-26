b1c501269cff82b4bb6b7c17d7b84bee26860411
liufan <liufan@google.com>
2022-11-26 13:51:37 +0800

    T

--- main.go
+++ main.go
@@ -44,9 +44,11 @@ func handler(w http.ResponseWriter, req *http.Request) {
 func play(input ArenaUpdate) (response string) {
 	log.Printf("IN: %#v", input)
 
-	commands := []string{"F", "R", "L", "T"}
-	rand := rand2.Intn(4)
-	
+	// commands := []string{"F", "R", "L", "T"}
+	// rand := rand2.Intn(4)
+
+
 	// TODO add your implementation here to replace the random response
-	return commands[rand]
+	// return commands[rand]
+	return "T"
 }
