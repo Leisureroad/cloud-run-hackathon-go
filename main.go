8f5b2c4711ca68dd9a01864c99b33b4ebdfe6232
liufan <liufan@google.com>
2022-11-26 17:50:33 +0800

    conflict

--- main.go
+++ main.go
@@ -48,70 +48,70 @@ func play(input ArenaUpdate) (response string) {
 	// r := reflect.ValueOf(input)
 	// f := reflect.Indirect(r).FieldByName("Arena")
 	// log.Printf("value: ", input.Arena.State["PlayerState"].X)
-	x :=input.Arena.State["https://cloud-run-hackathon-go-pkqx6rxn2q-uc.a.run.app"].X
-	y :=input.Arena.State["https://cloud-run-hackathon-go-pkqx6rxn2q-uc.a.run.app"].Y
-	d :=input.Arena.State["https://cloud-run-hackathon-go-pkqx6rxn2q-uc.a.run.app"].Direction
-
-
-	// direction :=input.Arena.State["PlayerState"].Direction
-	log.Printf("%i", x)
-	log.Printf("%i", y)
-	log.Printf("%v", d)
-  var xm int
-  var ym int
-	s :=input.Arena.State
-	for playerUrl, playerState := range s {
-	        fmt.Println("playerUrl:", playerUrl, "=>", "playerState:", playerState)
-					px :=playerState.X
-					py :=playerState.Y
-					if (x-px == 1) {
-					  xm = 1
-						log.Printf("%v", xm)
-				  }
-					if (y-py == 1) {
-						ym = 1
-						log.Printf("%v", ym)
-					}
-	    }
-	if (x != 0) {
-		if (d == "S" || d == "E") {
-	    return "R"
-		} else if (d == "N" && y == 0) {
-			return "L"
-		} else if (xm == 1 && y!= 0) {
-			    return "R"
-		} else {
-			commands := []string{"T", "F"}
-			rand := rand2.Intn(2)
-			return commands[rand]
-		}
-	}
-
-	if (y != 0) {
-		if (d == "N") {
-			commands := []string{"T", "F"}
-			rand := rand2.Intn(2)
-			return commands[rand]
-		} else {
-			return "R"
-		}
-	}
-
-	if (d == "E" ) {
-	  return "T"
-	} else {
-		return "R"
-	}
+	// x :=input.Arena.State["https://cloud-run-hackathon-go-pkqx6rxn2q-uc.a.run.app"].X
+	// y :=input.Arena.State["https://cloud-run-hackathon-go-pkqx6rxn2q-uc.a.run.app"].Y
+	// d :=input.Arena.State["https://cloud-run-hackathon-go-pkqx6rxn2q-uc.a.run.app"].Direction
+	//
+	//
+	// // direction :=input.Arena.State["PlayerState"].Direction
+	// log.Printf("%i", x)
+	// log.Printf("%i", y)
+	// log.Printf("%v", d)
+  // var xm int
+  // var ym int
+	// s :=input.Arena.State
+	// for playerUrl, playerState := range s {
+	//         fmt.Println("playerUrl:", playerUrl, "=>", "playerState:", playerState)
+	// 				px :=playerState.X
+	// 				py :=playerState.Y
+	// 				if (x-px == 1) {
+	// 				  xm = 1
+	// 					log.Printf("%v", xm)
+	// 			  }
+	// 				if (y-py == 1) {
+	// 					ym = 1
+	// 					log.Printf("%v", ym)
+	// 				}
+	//     }
+	// if (x != 0) {
+	// 	if (d == "S" || d == "E") {
+	//     return "R"
+	// 	} else if (d == "N" && y == 0) {
+	// 		return "L"
+	// 	} else if (xm == 1 && y!= 0) {
+	// 		    return "R"
+	// 	} else {
+	// 		commands := []string{"T", "F"}
+	// 		rand := rand2.Intn(2)
+	// 		return commands[rand]
+	// 	}
+	// }
+	//
+	// if (y != 0) {
+	// 	if (d == "N") {
+	// 		commands := []string{"T", "F"}
+	// 		rand := rand2.Intn(2)
+	// 		return commands[rand]
+	// 	} else {
+	// 		return "R"
+	// 	}
+	// }
+	//
+	// if (d == "E" ) {
+	//   return "T"
+	// } else {
+	// 	return "R"
+	// }
 
 	// log.Printf("%v", f)
 
 	// log.Printf("X: %#v", input.Arena.State.PlayerState["X"])
 
-	// commands := []string{"T", "F"}
-	// rand := rand2.Intn(2)
+	commands := []string{"L", "R", "T", "F"}
+	rand := rand2.Intn(4)
 
 
 	// TODO add your implementation here to replace the random response
-	// return commands[rand]
+	return commands[rand]
 	// return "T"
 }
